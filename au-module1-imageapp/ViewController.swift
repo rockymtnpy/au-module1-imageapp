cb71521d4e960da736307191f34edb7697dde954
rockymtnpy <119886264+rockymtnpy@users.noreply.github.com>
2025-05-25 21:21:45 -0600

    Add files via upload
    Initial commit

---
+++ au-module1-imageapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  au-module1-imageapp
+//
+//  Created by Brian Adair on 5/25/25.
+//  Copyright © 2025 Auburn University. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
